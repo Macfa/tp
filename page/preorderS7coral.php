37d87eeb03f32974007f38434e7fda59c6c08409
손혜현 <s.pepye@gmail.com>
2016-11-03 19:00:01 +0900

    s7엣지 블루코랄 랜딩페이지 코딩 s7엣지 블루코랄 메인배너 등록

---
+++ page/preorderS7coral.php
@@ -0,0 +1,9 @@
+<?
+require_once("./_common.inc.php");	// 공용부분 (모든 페이지에 쓰이는 php로직)
+
+$import->addCSS('preorderS7coral.css');
+$import->addJS('preorderS7coral.js');
+
+require_once($cfg['path']."/head.inc.php");			// 헤더 부분 (스킨포함)
+require_once("preorderS7coral.skin.php");	
+?>
