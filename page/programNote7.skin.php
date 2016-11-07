857a80f8cc0573ef115630b9fe9324ac34ba9691
손혜현 <s.pepye@gmail.com>
2016-11-07 12:06:54 +0900

    노트7 페이지 파일 등록

---
+++ page/programNote7.skin.php
@@ -0,0 +1,34 @@
+<div class="debug">
+    <label><input type="checkbox"> Debug</label>
+  </div>
+<div class="parallax">
+    <div id="group1" class="parallax__group">
+      <div class="parallax__layer parallax__layer--base">
+        <div class="title">참다참다 못 해 만든</br>티플 노트7 고객</br>구제 프로그램</div>
+      </div>
+    </div>
+    <div id="group2" class="parallax__group">
+      <div class="parallax__layer parallax__layer--base">
+        <div class="title"></div>
+      </div>
+      <div class="parallax__layer parallax__layer--back">
+        <div class="title">삼성의 노트7 보상안</div>
+      </div>
+    </div>
+    <div id="group3" class="parallax__group">
+      <div class="parallax__layer parallax__layer--fore">
+        <div class="title">도저히 답답해서 티플이 나섰습니다</br>노트7 고객구제 프로그램</div>
+      </div>
+      <div class="parallax__layer parallax__layer--base">
+        <div class="title"></div>
+      </div>
+    </div>
+    <div id="group4" class="parallax__group">
+      <div class="parallax__layer parallax__layer--base">
+        <div class="title"></div>
+      </div>
+      <div class="parallax__layer parallax__layer--deep">
+        <div class="title">자동 갤럭시 노트8</br>사전사전예약 실시</div>
+      </div>
+    </div>
+  </div>
