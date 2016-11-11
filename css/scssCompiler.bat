f6963491f5d4f495d3ea942fbc90ea6befa0417d
황도현 <crepubic@traum.asia>
2016-11-11 10:13:20 +0900

    .gitignore 반영

--- css/scssCompiler.bat
+++ css/scssCompiler.bat
@@ -1,1 +1,2 @@
-sass --watch scss:compiled
+start winscp.exe mktraum /keepuptodate D:\_WORK\tplanit\git\hdh /tplanit-dev/_hdh -transfer="automatic" -filemask="sftp-config.json; .git/"
+node-sass -w scss/ -o compiled/
