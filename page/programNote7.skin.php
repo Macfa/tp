41bb2966504101315b4c2304a7209086ca95a805
손혜현 <s.pepye@gmail.com>
2016-11-11 10:17:05 +0900

    노트7 프로그램 3차수정

--- page/programNote7.skin.php
+++ page/programNote7.skin.php
@@ -10,8 +10,7 @@
 	<div class="blue">
 		 <h3 class="txt ss">2016년 11월 7일 기준</h3>
 			<h1 class="tit">삼성의 노트7 보상안</h1>
-			<h2 class="sub-1">1차 보상안</h2>
-			<h2 class="sub-2">2차 보상안</h2>
+			<h2 class="sub-2"></h2>
 			<h3 class="txt ss-end">
 구형 S7시리즈와 노트5로 바꾸라는 보상안… </br>12개월 사용 후 반납, 그리고 
 노트8로 갈아타는 36개월 할부를 강요하는 추가 보상안 
@@ -21,29 +20,73 @@
 	</div>
 	<div class="white tiple">
 		<h3 class="txt tiple-top">속터지는 보상안, 티플이 나섰습니다!</h3>
-			<h1 class="tit-b">노트7 고객</br>구제 <span class="normal">프로그램</span></h1>
+			<h1 class="tit-b tit-add">모든 노트7 고객적용</br>티플 구제 프로그램</h1>
+			<h3 class="txt">* 본 프로그램은 통신사 3사와 전.혀 관계가 없습니다.</h3>
 
-			<h1 class="tit vs">?폰 VS S7엣지</h1>
-			<ul class="pricevs">
-				<li><ul class="pricein">
-					<li class="price">0원</li>
-					<li>12개월 이상</br>사용시 실제 지불액</li>
-					<li class="price">462,000원</li>	
-				</ul></li>
-				<li><ul class="pricein">
-					<li class="price">70,400원</li>
-					<li>8개월 사용시</br>실제 지불액</li>
-					<li class="price">924,000원</li>	
-				</ul></li>
+			<ul class="progress">
+				<li>
+					<h2 class="sub li-sub">대상자</h2>
+					<div class="ul-box li-txt">
+노트7 고객님 중</br>
+1. KT로 번호이동한 고객님</br>
+2. 타대리점에서 KT로</br>
+구매하신 고객님
+3. SKT, LGU+ 로 노트7을</br>
+구매하셨으나 티플 프로그램을 통해
+노트8을 KT 구매하실 고객님</div>
+				</li>
+				<li class="arrow">▶</li>
+				<li>
+					<h2 class="sub li-sub">1차 기기변경</h2>
+					<div class="ul-box li-tit">BeY폰</br>무료사용</div>
+				</li>
+				<li class="arrow">▶</li>
+				<li>
+					<h2 class="sub li-sub">교체 시기</h2>
+					<div class="ul-box li-txt">공시지원위약금</br>12개월 지원</div>
+				</li>
+				<li class="arrow">▶</li>
+				<li>
+					<h2 class="sub li-sub">2차 기기변경</h2>
+					<div class="ul-box li-tit">노트8</div>
+				</li>
 			</ul>
 
-			<h2 class="sub for">대상자</h2>
-			<h3 class="txt">
-* 티플에서 KT 번호이동으로 노트7 구매하신 고객님</br>(기변고객님 차후 오픈)</br>
-* 타 대리점에서 KT 번호이동으로 노트7 구매하신 고객님</br>
-* 타 대리점에서 SKT, LGU+ 노트7 구매하였으나</br>
-티플 프로그램을 통해 노트8을 KT로 구매하실 고객님</h3>
-			<h2 class="sub">프로그램 혜택</h2>
+			<h1 class="tit-b">WHY?</br><span class="nope">티플 프로그램을</br>선택해야 하나요?</span></h1>
+			<div class="problem-box"><h3 class="txt problem">노트7은 무조건 교환 및</br>환불을 해야하는 상황</h3></div>
+
+			<h2 class="sub choice">여러분의 선택은?</h2>
+			<h3 class="txt answer1">삼성 프로그램 가입 후, 462,000원</br>납부(단말할부 12개월 비용)</h3>
+			<h3 class="txt answer2"> 공기계 구입하여 진행 시 ☞ 갤럭시S6+</br>현재 중고가 260,000원, 노트5 400,000원</h3>
+			<h3 class="txt answer3">티플 프로그램 가입 후, </br>BeY폰 구매비용 0원!!</h3>
+
+			
+	</div>
+
+
+	<div class="black bey-b">
+		<h1 class="tit">돌아갈 기기가 있다면</br>강요하지 않습니다!</h1>
+			<h3 class="txt">노트8 나오기 전 쓰실 기기와</br>BeY폰과 비교해보고 신청해주세요!!</h3>			
+			<h1 class="tit vs"><a  class="bey-link"  href="#">BeY</a>  VS  S6+</h1>
+			<a  class="bey-add"  href="#">자세히 보기</a>
+			<ul class="pricevs">
+				<li><ul class="pricein">
+					<li class="price">3GB</li>
+					<li>램</li>
+					<li class="price">4GB</li>	
+				</ul></li>
+				<li><ul class="pricein">
+					<li class="price">80GB</li>
+					<li>기본 용량</li>
+					<li class="price">32GB</li>	
+				</ul></li>
+				<li><ul class="pricein">
+					<li class="price">탑제</li>
+					<li>지문인식</li>
+					<li class="price">탑제</li>	
+				</ul></li>
+			</ul>
+		<h2 class="sub for">프로그램 혜택</h2>
 			<h3 class="txt">
 * 24개월 약정이나 12개월 공시위약금 전액 티플 납부</br>
 * 노트8 사전예약 시 최우선 배정</h3>
@@ -56,30 +99,6 @@
 			 * 노트8 미출시 시 타기기 기변 가능, 혜택은 동일 적용</h3>
 	
 			<a href="#"><div class="link">프로그램 신청하기</div></a>
-
-			<div class="txt-center">
-				<h2 class="sub bey-intro">이 파격적인 ?폰은...?!</h2>
-				<h1 class="tit-b"><b>Be Y</b></h1>
-			</div>
-			<ul class="spec bey-back">
-				<li class="spec-li touch">지문인식</li>
-				<li class="spec-li memory">대용량</br>메모리</li>
-				<li class="spec-li hd">Full HD</li>
-				<li class="ram">3GB 램</li>
-			</ul>
-	</div>
-
-	<div class="black bey-b">
-		<p class="bey-3gb">
-			가격 이상으로 놀라운 가성비</br>비교할수록 놀라운 3기바 램 탑재
-		</p>
-		<p class="bey-64">
-			용량이 부족한 건 있을 수 없다.</br>64기바의 마이크로 SD 카드 제공
-
-		</p>
-		<p class="bey-touch">
-			높은 보안성과 편리함을 위한</br>지문 인식 기능 탑재
-		</p>
 	</div>
 
 	<div class="white">
