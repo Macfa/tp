731175776951e4741710ebdc3ad4c8b219d52e45
이민정 <kizunamj@traum.asia>
2016-11-28 16:23:59 +0900

    사은품추가페이지

--- product/dev-giftList.inc.php
+++ product/dev-giftList.inc.php
@@ -1,6 +1,6 @@
 <?
 if ($additialWhere == '')
-	$additialWhere = 'WHERE gfDisplay = 1';
+	$additialWhere = 'WHERE gfDisplay = 1 or gfKey = 137';
 $giftResults = DB::query("SELECT * FROM tmGift ".$additialWhere);
 $additialWhere = '';
 if ($isBig == true)
@@ -13,7 +13,24 @@ if ($startIndex == false && $startIndex !== 0)
 	$startIndex = 1;
 
 
-require("dev-giftList.skin.php");		
+foreach ($giftResults as $giftRow){
+
+	$thumbString[$giftRow['gfKey']] = DB::queryFirstField("SELECT gfThumb FROM tmGift WHERE gfKey=%i", $giftRow['gfKey']);
+
+	$tmp = $thumbString[$giftRow['gfKey']];	
+
+	$result[$giftRow['gfKey']] = substr($tmp, 0, 4);
+
+	if($result[$giftRow['gfKey']] === 'gift'){		
+		$gfThumbPath[$giftRow['gfKey']] = PATH_IMG."/";
+	}else{
+		$gfThumbPath[$giftRow['gfKey']] = PATH."/image.index.php?name=";
+	}
+	
+}
+
+require("dev-giftList.skin.php");
+
 $startIndex = 1;
 $isBig = false;
 $bigSuffix = '';
@@ -22,4 +39,7 @@ $groupCenterSuffix = '';
 $selectionList = FALSE;
 $giftResults = '';
 $giftListCfg = array();
+
+
+
 ?>
