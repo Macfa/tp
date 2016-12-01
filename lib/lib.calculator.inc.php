62da758689868d5c56327aae28f33f7d7ca19641
황도현 <crepubic@traum.asia>
2016-12-01 18:58:40 +0900

    배열 키 수정

--- lib/lib.calculator.inc.php
+++ lib/lib.calculator.inc.php
@@ -530,7 +530,7 @@ class planCalculator {
 		unset($data);
 
 		$data['content'] = $this->calculatorResult.$this->calculatorPad;
-		$data['dvId'] = $this->dvKey;
+		$data['dvKey'] = $this->dvKey;
 		$data['dvId'] = $this->dvId;
 		$data['token'] = createToken();
 		$calculator = getResultTemplate($data, $this->htmlCalculatorTemplate);
