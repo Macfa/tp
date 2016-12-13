79a26e0074329360c29e133c680f530abe371b47
황도현-서브 <crepubic@traum.asia>
2016-12-14 08:35:42 +0900

    에그프로모션 hotfix 마무리

---
+++ page/newyearEvent.php
@@ -0,0 +1,14 @@
+<?
+require_once("./_common.inc.php");	// 공용부분 (모든 페이지에 쓰이는 php로직)
+
+$add_css = '<link rel="stylesheet" href="'.PATH_CSS.'/newyearEvent.css" type="text/css">';
+
+$cutline = '2016-08-28 12:00:00';
+if ($cfg['time_ymdhis'] < $cutline) {
+	$affixSKapply = '('.getRelativeDate('2016-08-28').' 점심 12시 부터 가능)';
+}
+
+require_once($cfg['path']."/head.inc.php");			// 헤더 부분 (스킨포함)
+require_once("newyearEvent.skin.php");	
+require_once($cfg['path']."/foot.inc.php");			// foot 부분 (스킨포함)
+?>
---
+++ page/newyearEvent.skin.php
@@ -0,0 +1,58 @@
+<main>
+<section class="newyear2017-intro">
+	<div class="line"></div>
+	<h3 class="tit-deco">티플 2017 신년 이벤트</h3>
+	<div class="line"></div>
+	<h1 class="tit"><span class="letter-sp">2017</span></br>럭키보자<span class="so-small-txt">기</span></h1>
+	<h3 class="txt">선물 받고 2017년 모두 부자되세요!</br>티플에서 스마트폰 구매 시</br>15,000원 상당 럭키박스 증정!</h3>
+	<div class="highlight">
+		<h3 class="txt">2016년 12월 15일 ~ 2017년 2월 15일까지</h3>
+	</div>
+
+	<div class="highlight when">
+		<h3 class="txt">언제?&nbsp;&nbsp;&nbsp; ｜ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;개통 14일 이후 일괄발송!&nbsp;&nbsp;&nbsp;</h3>
+	</div>
+</section>
+<section class="newyear2017-presentlist">
+	<div class="short-box">
+		<div class="short-line"></div>
+		<div class="short-center"><h2 class="sub">럭키보자기</br>사은품 종류</h2></div>
+		<div class="short-line"></div>
+	</div>
+	<div class="present-list">
+		<ul>
+			<li class="fir">
+				<h3 class="txt list-txt">카카오프렌즈</br>캐릭터 케이블</h3>
+			</li>
+			<li class="sec">
+				<h3 class="txt list-txt">케이블정리</br>부착클립</h3>
+			</li>
+			<li class="thd">
+				<h3 class="txt list-txt">간단 사용!</br>보틀캡 가습기</h3>
+			</li>
+			<li class="for">
+				<h3 class="txt list-txt">5/8핀 겸용</br>멀티 케이블</h3>
+			</li>
+			<li class="fif">
+				<h3 class="txt list-txt">벽면부착형</br>핸드폰 홀더</h3>
+			</li>
+			<li class="six">
+				<h3 class="txt list-txt">아이폰7 필수템</br>멀티 케이블</h3>
+			</li>
+			<li class="sev">
+				<h3 class="txt list-txt">지라이더</br>스마트캐치</h3>
+			</li>
+			<li class="eig">
+				<h3 class="txt list-txt">케이블 내장형</br>배터리 5000mAh</h3>
+			</li>
+			<li class="nin">
+				<h3 class="txt list-txt">동그란</br>동그라미</h3>
+			</li>
+			<li class="ten">
+				<h3 class="txt list-txt">동그란</br>동그라미</h3>
+			</li>
+			
+		</ul>		
+	</div>
+</section>
+</main>
