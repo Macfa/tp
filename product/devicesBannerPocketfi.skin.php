9d452db87cefae3dc416aaccafc0fbd41c841f80
손혜현 <s.pepye@gmail.com>
2016-12-13 19:05:01 +0900

    에그 프로모션 배너 교체

--- product/devicesBannerPocketfi.skin.php
+++ product/devicesBannerPocketfi.skin.php
@@ -1,10 +1,10 @@
 <main class="list-banner-pocketfi">
 	<a href="/product/egg.php" class="list-banner-inner-pocketfi" id="link-banner-pocketfi">
-		<h1 class="list-banner-tit">
+		<!--<h1 class="list-banner-tit">
 			데이터를 언제 어디든지<br/>꺼내쓰는 현명한 방법
 			<br/>
 			LTE egg + A
 		</h1>
-		<button class="btn">엄청한 사은품과 함께 구매하기</button>
+		<button class="btn">엄청한 사은품과 함께 구매하기</button>-->
 	</a>
 </main>
