2bc2d76de781fcf35f4c69f0617b7568f14b7f08
이민정 <kizunamj@traum.asia>
2016-12-15 17:02:04 +0900

    추천포인트추가 action

--- product/applyAction.php
+++ product/applyAction.php
@@ -32,10 +32,51 @@ try
 	if(isNullVal($_POST['apColor']))
 		throw new Exception('색상을 선택해주세요 ', 3);
 
+	if(isNullVal($_POST['apPhone']))
+		throw new Exception('추가전화번호를 적어주세요. ', 3);
+
+	if(isNullVal($_POST['apBirth']))
+		throw new Exception('생년월일을 적어주세요. ', 3);
+
 	$isExistDevice = DB::queryFirstField("SELECT dvKey FROM tmDevice WHERE dvDisplay = 1 and dvKey = %s", $_POST['dvKey']);
 
 	if($isExistDevice === FALSE)
 		throw new Exception('존재하지 않는 기기입니다.', 3);
+
+	if(isExist($_POST['recommedID'])){
+
+		$myMbKey = $mb['mbKey'];
+
+		// 추천인 정보 조회
+		list($isMemberExist, $targetMbKey, $targetMbEmail, $targetMbPoint) = DB::queryFirstList("SELECT COUNT(*), mbKey, mbEmail, mbPoint FROM tmMember WHERE mbEmail=%s", $_POST['recommedID']);	
+		$isMemberExist = (int)$isMemberExist;
+
+		if($isMemberExist === 0)
+			throw new Exception("멤버가 존재하지 않습니다");	
+		if($targetMbKey === $myMbKey)
+			throw new Exception("본인추천은 불가능합니다");
+
+		//본인이 mbkey 일때 parent,grand 정보
+		//$isRelationExist : 본인이 mbkey 일때의 행이 존재하는 count
+		list($isRelationExist, $isMyKey, $prParent,  $prGrand )= DB::queryFirstList("SELECT COUNT(*), mbKey, prParent, prGrand FROM tmPointRelationship WHERE mbKey=%i",$myMbKey);	
+
+		//본인이 parent 일때 mbkey,grand 정보
+		// $isExistRowWhenParent : 내가 부모인 행이 존재하는 행의 count
+		// $prGrandWhenParent : 내가 부모인 행에서 prGrand 값		
+		list($isExistRowWhenParent, $prGrandWhenParent, $childKey)= DB::queryFirstList("SELECT COUNT(*), prGrand, mbKey FROM tmPointRelationship WHERE prParent=%i",$myMbKey);
+
+		if($prParent === $targetMbKey) $targetIsAlreadyParents = true;
+		if($targetIsAlreadyParents === false && $isRelationExist >= 1)  
+			throw new Exception("이미 다른 추천인이 등록되어있습니다"); 
+
+		if($myMbKey === $isMyKey && $prParent !== $targetMbKey) // 다른 추천인을 넣었을때
+			throw new Exception("이미 다른 추천인이 등록되어있습니다"); 
+
+		if($childKey === $targetMbKey)
+			throw new Exception("추천해준 사람을 추천할 수 없습니다");
+
+	}
+
 
 
 }
@@ -71,6 +112,72 @@ DB::update('tmMember', array(
 	'mbEmail = %s', $mb['mbEmail']
 );
 
+
+
+//////////////////추천포인트 지급
+
+if(isExist($_POST['recommedID'])){//추천포인트 지급
+
+	// ID - parent 관계 인서트 - 포인트지급
+	$isRelationExist = (int)$isRelationExist;
+	if($isRelationExist === 0){
+		DB::insert('tmPointRelationship', array(
+		  'mbKey' => $myMbKey, 
+		  'prParent' => $targetMbKey,
+		  'prDate' => $cfg['time_ymdhis']
+		));	
+	}
+
+	DB::update('tmMember', 
+		array(
+			'mbPoint' => DB::sqleval("mbPoint+($rewardPoint * 0.1)")
+		),	'mbKey = %i', $targetMbKey
+	);
+
+	DB::insert('tmPointHistory', array(
+		'mbEmail' => $targetMbEmail,
+		'phCont' => $cfg['time_ymdhis'].' 추천포인트지급',
+		'phAmount' => $rewardPoint*0.05,
+		'phResult' => $targetMbPoint + ($rewardPoint*0.05) ,
+		'phDate' => $cfg['time_ymdhis']
+	));
+
+
+	// B | A | 0 에서
+	// A 가 C를 추천인으로 임명했을때
+
+	// B | A | C
+	// A | C | 0
+	// 이 구조가 되게 해주는 코드
+	// parent - grand 관계 인서트
+	$prGrandWhenParent = (int)$prGrandWhenParent;
+	if($isExistRowWhenParent >= 1){
+		if($prGrandWhenParent === 0){		
+			DB::update('tmPointRelationship', array(
+			  'prGrand' => $targetMbKey 
+			), "prParent=%i", $myMbKey);
+		}
+	}else if ($isRelationExist === 1 && isExist($prGrand) === true ) { // 3단계 관계가 성립할때 3단계 포인트 지급 
+
+		list($grandMbEmail, $grandMbPoint) = DB::queryFirstList("SELECT mbEmail, mbPoint FROM tmMember WHERE mbKey=%i", $prGrand); //grand 추천인 정보
+
+		DB::update('tmMember', 
+			array(
+				'mbPoint' => DB::sqleval("mbPoint+($rewardPoint*0.05)")
+			),	'mbKey = %i', $prGrand
+		);
+
+		DB::insert('tmPointHistory', array(
+			'mbEmail' => $grandMbEmail,
+			'phCont' => $cfg['time_ymdhis'].' 3단계추천포인트지급',
+			'phAmount' => $rewardPoint*0.05,
+			'phResult' => $grandMbPoint + ($rewardPoint*0.05) ,
+			'phDate' => $cfg['time_ymdhis']
+		));
+
+	}
+}
+
 $cdCode = DB::queryFirstField("SELECT cdCode FROM tmCode WHERE dvKey = %i_dvKey and spPlan = %i_spPlan and cdType = %i_cdType and cdCarrier = %s_cdCarrier", 
 	array('dvKey'=> $_POST['dvKey'], 
 			'spPlan' => $_POST['plan'], 
@@ -78,8 +185,6 @@ $cdCode = DB::queryFirstField("SELECT cdCode FROM tmCode WHERE dvKey = %i_dvKey 
 			'cdCarrier' => $_POST['carrier']
 	)
 );
-
-
 
 $deviceInfo = new deviceInfo();
 $deviceInfo->setCarrier($_POST['carrier']);
