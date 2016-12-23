d86224579c6fa1bb145dbfd16a9c1a0cdd02d977
손혜현 <s.pepye@gmail.com>
2016-12-23 11:46:45 +0900

    제외

--- css/scssCompiler.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start winscp.exe mktraum /keepuptodate D:\_WORK\tplanit\git\hdh /tplanit-dev/_hdh -transfer="automatic" -filemask="sftp-config.json; .git/"  -delete
-:RETURN
-node-sass scss/ -o compiled/ & node-sass -w scss/ -o compiled/ || goto RETURN
